81682567e92c2011b852c9a28316fce02c198f8c
kuboD0507 <72774967+kuboD0507@users.noreply.github.com>
2020-11-05 16:06:22 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,10 @@
 // 小手調べ
-function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+function setup() {
+  createCanvas(400, 400);
+  for(let i=200; i>0; i-=20){
+    if(i>100){
+  stroke(255,0,0)}
+    else{stroke(0,0,255)}
+  ellipse(200,200,i);
   }
 }
